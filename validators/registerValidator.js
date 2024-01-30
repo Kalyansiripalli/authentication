618c928fac99bcd8a92a63c445ae125e32603248
Kalyansiripalli <siripallivenkatakalyan@gmail.com>
2024-01-30 11:15:46 +0530

    authentication

--- validators/registerValidator.js
+++ validators/registerValidator.js
@@ -6,6 +6,7 @@ body("firstname").isString().isLength({min:3}).not().isEmpty(),
 body('lastname').isString().isLength({min:3}).not().isEmpty(),
 body('email').isEmail(),
 body('password').isString().isLength({min:3}).not().isEmpty(),
+// js
 body('contact').isString().isLength({min:11}).optional(),
 (req,res,next)=>{
     const errors = validationResult(req);
